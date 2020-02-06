47bf03ceb8a542ddf45908dd50d31f3bbb80e63b
Artem <56476391+HillKTON@users.noreply.github.com>
2020-02-06 23:09:37 +0500

    Добавлен файл keygen.py
    А вот и генератор собственной персоны

---
+++ keygen.py
@@ -0,0 +1,16 @@
+import string
+import random
+
+def generate(count):
+    out = str()
+    for _ in range(count):
+        keys = []
+        for _ in range(3):
+            keys.append(''.join((random.choice(string.ascii_uppercase + string.digits) for _ in range(5))))
+        out += '-'.join(keys) + '\n'
+    return out[:-2]
+
+if __name__ == '__main__':
+    count = int(input('Укажите нужное кол-во ключей: '))
+    keys = generate(count)
+    print(keys)
