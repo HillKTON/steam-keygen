55163be007b3322753674147b9265e1d5faff974
HillKTON <56476391+HillKTON@users.noreply.github.com>
2020-02-08 18:23:15 +0500

    Внесение поправок

--- keygen.py
+++ keygen.py
@@ -1,6 +1,7 @@
 import string
 import random
 
+
 def generate(count):
     out = str()
     for _ in range(count):
@@ -8,7 +9,8 @@ def generate(count):
         for _ in range(3):
             keys.append(''.join((random.choice(string.ascii_uppercase + string.digits) for _ in range(5))))
         out += '-'.join(keys) + '\n'
-    return out[:-2]
+    return out[:-1]
+
 
 if __name__ == '__main__':
     count = int(input('Укажите нужное кол-во ключей: '))
